14c70f0d67f84872e6c0fda6daa2040361f4800f
vladboyr <vladboyr@gmail.com>
2023-04-24 00:48:04 +0500

    HotKey - Ctrl+Alt+J

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import keyboard
 import sys
 from PyQt6.QtWidgets import QApplication, QDialog
 from ui_imagedialog import Ui_ImageDialog
+
+keyboard.add_hotkey("ctrl+alt+j", lambda: print("ctrl+alt+j was pressed"))
 
 app = QApplication(sys.argv)
 window = QDialog()
